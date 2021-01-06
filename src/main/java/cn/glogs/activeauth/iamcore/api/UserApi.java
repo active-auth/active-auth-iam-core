9f016915d1770d0481974f827eabc70819c57059
Dionysus <dengyijian163@163.com>
2021-01-06 19:34:48 +0800

    feature: policy grants in API.

--- src/main/java/cn/glogs/activeauth/iamcore/api/UserApi.java
+++ src/main/java/cn/glogs/activeauth/iamcore/api/UserApi.java
@@ -1,8 +1,12 @@
 package cn.glogs.activeauth.iamcore.api;
 
+import cn.glogs.activeauth.iamcore.api.helper.AuthCheckingHelper;
+import cn.glogs.activeauth.iamcore.api.payload.AuthCheckingContext;
+import cn.glogs.activeauth.iamcore.api.payload.AuthCheckingStatement;
 import cn.glogs.activeauth.iamcore.api.payload.RestResultPacker;
 import cn.glogs.activeauth.iamcore.domain.AuthenticationPrincipal;
 import cn.glogs.activeauth.iamcore.domain.AuthenticationSession;
+import cn.glogs.activeauth.iamcore.domain.AuthorizationPolicyGrant;
 import cn.glogs.activeauth.iamcore.domain.password.PasswordHashingStrategy;
 import cn.glogs.activeauth.iamcore.exception.HTTP401Exception;
 import cn.glogs.activeauth.iamcore.exception.HTTP404Exception;
@@ -10,21 +14,31 @@ import cn.glogs.activeauth.iamcore.exception.HTTPException;
 import cn.glogs.activeauth.iamcore.exception.business.NotFoundException;
 import cn.glogs.activeauth.iamcore.service.AuthenticationPrincipalService;
 import cn.glogs.activeauth.iamcore.service.AuthenticationSessionService;
+import cn.glogs.activeauth.iamcore.service.AuthorizationPolicyGrantService;
+import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequestMapping("/user-center")
 @RestController
 public class UserApi {
     private final AuthenticationPrincipalService authenticationPrincipalService;
     private final AuthenticationSessionService authenticationSessionService;
+    private final AuthorizationPolicyGrantService authorizationPolicyGrantService;
+    private final AuthCheckingHelper authCheckingHelper;
 
-    public UserApi(AuthenticationPrincipalService authenticationPrincipalService, AuthenticationSessionService authenticationSessionService) {
+    public UserApi(
+            AuthenticationPrincipalService authenticationPrincipalService,
+            AuthenticationSessionService authenticationSessionService,
+            AuthorizationPolicyGrantService authorizationPolicyGrantService,
+            AuthCheckingHelper authCheckingHelper
+    ) {
         this.authenticationPrincipalService = authenticationPrincipalService;
         this.authenticationSessionService = authenticationSessionService;
+        this.authorizationPolicyGrantService = authorizationPolicyGrantService;
+        this.authCheckingHelper = authCheckingHelper;
     }
 
     @PostMapping("/login")
@@ -48,4 +62,12 @@ public class UserApi {
         );
         return RestResultPacker.success(authenticationPrincipalService.createPrincipal(toCreatePrincipal).vo());
     }
+
+    @GetMapping("/grants/in")
+    public RestResultPacker<Page<AuthorizationPolicyGrant.Vo>> pagingGrantsIn(HttpServletRequest request, @RequestParam int page, @RequestParam int size) throws HTTPException {
+        AuthCheckingContext authCheckingContext = authCheckingHelper.myResources(request, AuthCheckingStatement.checks("iam:GetGrant", "iam://users/%s/grants-in"));
+        Page<AuthorizationPolicyGrant> grantsPage = authorizationPolicyGrantService.pagingGrantsTo(authCheckingContext.getCurrentSession().getAuthenticationPrincipal(), page, size);
+        return RestResultPacker.success(grantsPage.map(AuthorizationPolicyGrant::vo));
+    }
+
 }
