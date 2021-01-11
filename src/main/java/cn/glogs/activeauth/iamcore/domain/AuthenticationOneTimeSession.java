e6cf26bb936d3ac6a798577a3b5d50d0959b4bac
Okeyja Teung <dengyijian163@163.com>
2021-01-11 20:47:10 +0800

    feature: onetimesession.

--- src/main/java/cn/glogs/activeauth/iamcore/domain/AuthenticationOneTimeSession.java
+++ src/main/java/cn/glogs/activeauth/iamcore/domain/AuthenticationOneTimeSession.java
@@ -1,12 +1,12 @@
 package cn.glogs.activeauth.iamcore.domain;
 
+import cn.glogs.activeauth.iamcore.domain.converter.StringListAttributeConverter;
 import lombok.Data;
+import org.apache.commons.lang3.RandomStringUtils;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @Entity
@@ -14,7 +14,35 @@ public class AuthenticationOneTimeSession {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    private AuthenticationPrincipal principal;
+
     private String token;
+
+    private String action;
+
+    @Convert(converter = StringListAttributeConverter.class)
+    private List<String> resources;
+
+    private boolean ruined;
+
     private Date createdAt;
-    private Date expiredAt;
+
+    private Date ruinedAt;
+
+    public static AuthenticationOneTimeSession create(AuthenticationPrincipal principal, String action, List<String> resources) {
+        AuthenticationOneTimeSession oneTimeSession = new AuthenticationOneTimeSession();
+        oneTimeSession.principal = principal;
+        oneTimeSession.token = RandomStringUtils.randomAlphanumeric(72);
+        oneTimeSession.action = action;
+        oneTimeSession.resources = resources;
+        oneTimeSession.ruined = false;
+        return oneTimeSession;
+    }
+
+    public void ruin() {
+        this.ruined = true;
+        this.ruinedAt = new Date();
+    }
 }
