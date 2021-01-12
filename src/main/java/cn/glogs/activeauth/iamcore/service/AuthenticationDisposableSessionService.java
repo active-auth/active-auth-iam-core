f166143792445002fc92df2f61381339eb447b87
Okeyja Teung <dengyijian163@163.com>
2021-01-12 09:36:51 +0800

    feature: create disposable session by principalId.

--- src/main/java/cn/glogs/activeauth/iamcore/service/AuthenticationDisposableSessionService.java
+++ src/main/java/cn/glogs/activeauth/iamcore/service/AuthenticationDisposableSessionService.java
@@ -2,10 +2,13 @@ package cn.glogs.activeauth.iamcore.service;
 
 import cn.glogs.activeauth.iamcore.domain.AuthenticationDisposableSession;
 import cn.glogs.activeauth.iamcore.domain.AuthenticationPrincipal;
+import cn.glogs.activeauth.iamcore.exception.business.NotFoundException;
 
 import java.util.List;
 
 public interface AuthenticationDisposableSessionService {
+    AuthenticationDisposableSession create(Long principalId, List<String> actions, List<String> resources) throws NotFoundException;
+
     AuthenticationDisposableSession create(AuthenticationPrincipal principal, List<String> actions, List<String> resources);
 
     boolean consume(String token);
--- src/main/java/cn/glogs/activeauth/iamcore/service/impl/AuthenticationDisposableSessionServiceImpl.java
+++ src/main/java/cn/glogs/activeauth/iamcore/service/impl/AuthenticationDisposableSessionServiceImpl.java
@@ -2,9 +2,12 @@ package cn.glogs.activeauth.iamcore.service.impl;
 
 import cn.glogs.activeauth.iamcore.domain.AuthenticationDisposableSession;
 import cn.glogs.activeauth.iamcore.domain.AuthenticationPrincipal;
+import cn.glogs.activeauth.iamcore.exception.business.NotFoundException;
 import cn.glogs.activeauth.iamcore.repository.AuthenticationDisposableSessionRepository;
+import cn.glogs.activeauth.iamcore.repository.AuthenticationPrincipalRepository;
 import cn.glogs.activeauth.iamcore.service.AuthenticationDisposableSessionService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,19 +15,31 @@ import java.util.Optional;
 @Service
 public class AuthenticationDisposableSessionServiceImpl implements AuthenticationDisposableSessionService {
 
+    private final AuthenticationPrincipalRepository authenticationPrincipalRepository;
     private final AuthenticationDisposableSessionRepository authenticationDisposableSessionRepository;
 
-    public AuthenticationDisposableSessionServiceImpl(AuthenticationDisposableSessionRepository authenticationDisposableSessionRepository) {
+    public AuthenticationDisposableSessionServiceImpl(AuthenticationPrincipalRepository authenticationPrincipalRepository, AuthenticationDisposableSessionRepository authenticationDisposableSessionRepository) {
+        this.authenticationPrincipalRepository = authenticationPrincipalRepository;
         this.authenticationDisposableSessionRepository = authenticationDisposableSessionRepository;
     }
 
     @Override
+    @Transactional
+    public AuthenticationDisposableSession create(Long principalId, List<String> actions, List<String> resources) throws NotFoundException {
+        AuthenticationPrincipal principal = authenticationPrincipalRepository.findById(principalId).orElseThrow(() -> new NotFoundException(String.format("Principal %s not found.", principalId)));
+        AuthenticationDisposableSession toSave = AuthenticationDisposableSession.generate(principal, actions, resources);
+        return authenticationDisposableSessionRepository.save(toSave);
+    }
+
+    @Override
+    @Transactional
     public AuthenticationDisposableSession create(AuthenticationPrincipal principal, List<String> actions, List<String> resources) {
         AuthenticationDisposableSession toSave = AuthenticationDisposableSession.generate(principal, actions, resources);
         return authenticationDisposableSessionRepository.save(toSave);
     }
 
     @Override
+    @Transactional
     public boolean consume(String token) {
         Optional<AuthenticationDisposableSession> disposableSessionOpt = authenticationDisposableSessionRepository.findByToken(token);
         if (disposableSessionOpt.isPresent()) {
