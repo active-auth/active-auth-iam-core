00dff43cdfa791fb4a80ae44f794715d1d349c53
Okeyja Teung <dengyijian163@163.com>
2021-01-12 09:31:22 +0800

    save: delete unused import.

--- src/main/java/cn/glogs/activeauth/iamcore/config/SpringDoc.java
+++ src/main/java/cn/glogs/activeauth/iamcore/config/SpringDoc.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OpenApiCustomiser;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
