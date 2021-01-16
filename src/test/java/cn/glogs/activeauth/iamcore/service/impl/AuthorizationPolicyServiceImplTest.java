3f8a6b44b065cf6ae1bc1e4732ef1adb41cf04e2
Okeyja Teung <dengyijian163@163.com>
2021-01-16 22:12:48 +0800

    test: edit policy test.

--- src/test/java/cn/glogs/activeauth/iamcore/service/impl/AuthorizationPolicyServiceImplTest.java
+++ src/test/java/cn/glogs/activeauth/iamcore/service/impl/AuthorizationPolicyServiceImplTest.java
@@ -1,34 +1,61 @@
 package cn.glogs.activeauth.iamcore.service.impl;
 
+import cn.glogs.activeauth.iamcore.domain.AuthenticationPrincipal;
 import cn.glogs.activeauth.iamcore.domain.AuthorizationPolicy;
-import org.dozer.DozerBeanMapper;
-import org.junit.jupiter.api.Assertions;
+import cn.glogs.activeauth.iamcore.domain.password.PasswordHashingStrategy;
+import cn.glogs.activeauth.iamcore.exception.business.NotFoundException;
+import cn.glogs.activeauth.iamcore.repository.AuthenticationPrincipalRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static cn.glogs.activeauth.iamcore.domain.AuthorizationPolicy.PolicyEffect.ALLOW;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class AuthorizationPolicyServiceImplTest {
 
     @Autowired
-    private DozerBeanMapper dozerBeanMapper;
+    private AuthenticationPrincipalRepository principalRepository;
+
+    @Autowired
+    private AuthorizationPolicyServiceImpl policyService;
+
+    private AuthenticationPrincipal sampleOwner;
+
+    @BeforeEach
+    void init() {
+        AuthenticationPrincipal toSavePrincipal = new AuthenticationPrincipal("mayun", "IAmMaYun", AuthenticationPrincipal.PrincipalType.PRINCIPAL, PasswordHashingStrategy.B_CRYPT);
+        this.sampleOwner = principalRepository.save(toSavePrincipal);
+    }
 
     @Test
-    void dozerMappingTest() {
-        AuthorizationPolicy.Form form = new AuthorizationPolicy.Form(
-                "test policy", ALLOW,
-                List.of("sample:Action1", "sample:Action2"),
-                List.of("arn:cloudapp:bookshelf::31:bought-book/*", "arn:cloudapp:bookshelf::31:shoppping-cart/*")
+    @Transactional
+    AuthorizationPolicy addPolicyTest() {
+        AuthorizationPolicy.Form sampleForm = new AuthorizationPolicy.Form(
+                "JackMa's Principal - 1", ALLOW,
+                List.of("bookshelf:AddBooks"),
+                List.of("arn:cloudapp:bookshelf::31:bought-book/*")
         );
-        AuthorizationPolicy policy = dozerBeanMapper.map(form, AuthorizationPolicy.class);
-        Assertions.assertEquals(form.getName(), policy.getName());
-        Assertions.assertEquals(form.getEffect(), policy.getEffect());
-        Assertions.assertArrayEquals(form.getActions().toArray(), policy.getActions().toArray());
-        Assertions.assertArrayEquals(form.getResources().toArray(), policy.getResources().toArray());
+        return policyService.addPolicy(sampleForm, this.sampleOwner);
+    }
+
+    @Test
+    @Transactional
+    void editPolicyTest() throws NotFoundException {
+        AuthorizationPolicy toEditPolicy = addPolicyTest();
+        AuthorizationPolicy.Form sampleForm = new AuthorizationPolicy.Form(
+                "JackMa's Principal - 2", ALLOW,
+                List.of("bookshelf:AddBooks", "bookshelf:DeleteBooks"),
+                List.of("arn:cloudapp:bookshelf::31:bought-book/*", "arn:cloudapp:bookshelf::31:favorites/*")
+        );
+        policyService.editPolicy(toEditPolicy.getId(), sampleForm, this.sampleOwner);
+        assertThrows(NotFoundException.class, () ->
+                policyService.editPolicy(toEditPolicy.getId() + 1, sampleForm, this.sampleOwner)
+        );
     }
 }
